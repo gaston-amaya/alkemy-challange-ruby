a80cbc71e7349c4d64628625e070163891c64381
gaston-amaya <amayagaston.sk@gmail.com>
2022-05-12 16:32:05 -0300

    added all 3 models + associations

---
+++ db/migrate/20220512182003_create_characters.rb
@@ -0,0 +1,13 @@
+class CreateCharacters < ActiveRecord::Migration[7.0]
+  def change
+    create_table :characters do |t|
+      t.string :picture
+      t.string :name
+      t.integer :age
+      t.integer :weight
+      t.string :story
+
+      t.timestamps
+    end
+  end
+end
